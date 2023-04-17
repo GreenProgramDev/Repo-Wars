839eea23de2f390cf8e375d774f10ad791fcd901
Sdney Lima <lima.rosimao@gmail.com>
2023-04-17 04:16:19 -0300

    feat: add basic structure of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import "./app.css"
 
 
 const App = () => {
-  return <div>App</div>
+  const [showGame, setShowGame] = useState<boolean>(false)
+  const [repos, setRepos] = useState( ["repol"] )
+  const handleStartClick = () => { setShowGame(true)  }
+
+  return (
+    <main>
+      {!showGame && (
+        <>
+        <h1>Welcome to RepoWars</h1>
+        <h2>A Developer-driven guessing game</h2>
+        {repos.length === 0 ? <p>Loading game...</p> : <button type="submit" onClick={handleStartClick} >Start</button>}
+          
+
+        </>
+      )}
+      {showGame && (
+        <>
+        <h1>Game</h1>
+        </>
+      )}
+  </main>
+  )
 }
 
 export default App
