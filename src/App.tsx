ccb613569a14f5907f4053a213ea20b54d29dd09
Sdney Lima <lima.rosimao@gmail.com>
2023-04-18 23:33:25 -0300

    feat: Add App Component Layout And style

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import "./app.css"
 const App = () => {
   const [showGame, setShowGame] = useState<boolean>(false)
   const [repos, setRepos] = useState( ["repol"] )
-  const handleStartClick = () => { setShowGame(true)  }
+  const handleStartClick = () => { setShowGame(true) }
 
   return (
     <main>
       {!showGame && (
         <>
-        <h1>Welcome to RepoWars</h1>
-        <h2>A Developer-driven guessing game</h2>
-        {repos.length === 0 ? <p>Loading game...</p> : <button type="submit" onClick={handleStartClick} >Start</button>}
-          
-
+          <h1 className='title'>Welcome to RepoWars</h1>
+          <h2 className='headline gradient text'>A Developer-driven guessing game</h2>
+          {repos.length === 0 ? (
+            <p className='loading'>Loading game...</p>
+          ) : (
+            <button className="button gradient" type="submit" onClick={handleStartClick} >Start</button>
+          )}
         </>
       )}
       {showGame && (
