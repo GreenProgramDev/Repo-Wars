da96455ed48034e63898916e1ae768040f8052a6
Sdney Lima <lima.rosimao@gmail.com>
2023-04-23 22:01:20 -0300

    feat: add Game logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import './game.css' 
 import RepoCard from "./RepoCard"
-import { FaExchangeAlt } from 'react-icons/fa'
+import { FaExchangeAlt, FaTimesCircle } from 'react-icons/fa'
 
 interface GameProps {
     repoState: [Repo[], React.Dispatch<React.SetStateAction<Repo[]>>]
@@ -15,19 +15,64 @@ const Game: React.FC<GameProps> = ( {
     originalList,
     setShowGame,
 }) => {
+    const [repositories, setRepositories] = repoState
     const [over, setOver] = useState<boolean>(false)
     const [repo2, setRepo2] = useState<Repo | null>(null)
     const [repo1, setRepo1] = useState<Repo | null>(null)
-    // TODO: use actual random repos
+    const [correct, setCorrect] = useState<number>(0)
+    const [wrong, setWrong] = useState<number>(0)
+    // Random Repo Selection Logic
+    const randomSelect = (arr: Repo[]) => {
+        // If there are less than 2 repositories, reset the list
+        if (arr.length < 2){
+            setRepositories(originalList)
+            setOver(true)
+            return
+        }
+        // Select a random index from the array
+        const generateRandom = () => Math.floor(Math.random() * arr.length)
+        const randomIndex1 = Math.floor(Math.random() * arr.length)
+        let randomIndex2 = Math.floor(Math.random() * arr.length)
+        
+        while (randomIndex1 === randomIndex2){
+            randomIndex2 = generateRandom()
+        }
+        //Select the two repositories with those indexes 
+        const randomRepo1 = arr[randomIndex1] 
+        const randomRepo2 = arr[randomIndex2] 
+        //Remove the two repositories from the array
+        const updatedArr = arr.filter(
+            repo=>repo.id !== randomRepo1.id && repo.id !== randomRepo2.id,
+            )    
+
+            //Set repos with the repo values
+
+            setRepo1(randomRepo1)
+            setRepo2(randomRepo2)
+
+            // Update the array
+            setRepositories(updatedArr)
+    }
+
+    //Set initial repos
+
     useEffect( () => {
-        setRepo1(originalList[0]);
-        setRepo2(originalList[1]);
+        randomSelect(repositories)
     }, [])
 
     // Handelers
 
     const handleChoice = (r: Repo) => {
+        const chosenRepo = r.id === repo1?.id ? repo1 : repo2
+        const winner = repo1?.stargazers_count! > repo2?.stargazers_count! ? repo1 : repo2
 
+        if (chosenRepo?.id === winner?.id) {
+            setCorrect((prev) => prev + 1)
+        }else{
+            setWrong((prev) => prev + 1)
+        }
+
+        randomSelect(repositories)
     }
 
     return (
@@ -41,7 +86,19 @@ const Game: React.FC<GameProps> = ( {
                        <RepoCard content={repo1} handler={handleChoice} />
 
                         <div className="dashboard">
+                            <div className="result correct">
+                                <label>Correct</label>
+                                <p>{correct}</p>
+                                <FaTimesCircle/>
+                            </div>
+
                             <FaExchangeAlt className="icon-versus"/>
+
+                            <div className="result wrong">
+                                <label>Wrong</label>
+                                <p>{wrong}</p>
+                                <FaTimesCircle/>
+                            </div>
                         </div>
 
                         <RepoCard content={repo2} handler={handleChoice}/>
@@ -57,7 +114,7 @@ const Game: React.FC<GameProps> = ( {
                     <button type="submit" 
                     className="button gradient" 
                     // TODO: change this button to refresh the current repositores
-                    onClick={() =>setShowGame(false)}>
+                    onClick={() => randomSelect(repositories) }>
                         Skip
                     </button>
                 )}
