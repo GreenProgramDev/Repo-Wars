f29afcda04b47d6b1f7fbd53db1697ababee41cb
Sdney Lima <lima.rosimao@gmail.com>
2023-04-30 23:01:20 -0300

    fix: gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Repo-Wars',
+  // base: '/Repo-Wars',
   plugins: [ react() ],
   resolve: { alias: { 'node-fetch' : 'axios' } },
     /*define: {
